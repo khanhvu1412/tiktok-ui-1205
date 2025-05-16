7a456be7b7e50ba2a777dd589ae6e7f6b195b156
DESKTOP-SPBBU7G\Hitech <dkv1412@gmail.com>
2025-05-16 11:09:26 +0700

    Xây dựng Logic phần Header #1

--- src/components/AccountsItem/index.js
+++ src/components/AccountsItem/index.js
@@ -1,6 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
+import Image from '~/components/Image';
 import styles from './AccountItem.module.scss';
 
 const cx = classNames.bind(styles);
@@ -8,7 +9,7 @@ const cx = classNames.bind(styles);
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/c14a92a8e2e23babececab98c0f67fac~tplv-tiktokx-cropcenter:1080:1080.jpeg?dr=14579&refresh_token=3d96d981&x-expires=1747368000&x-signature=1JDm7agg07weALi7StMJYLrBr8k%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=81f88b70&idc=my"
                 alt="Hoaa"
